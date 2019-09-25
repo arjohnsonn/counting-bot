f15cacd400cd6775302f446f47a168bfb921ef5a
RapidFX <52363894+RapidFX@users.noreply.github.com>
2019-09-24 21:10:11 -0500

    Update countingbot.py

--- countingbot.py
+++ countingbot.py
@@ -23,7 +23,7 @@ async def on_message(message):
         await client.logout()
 
 async def counter(max, message):
-    count = 282095
+    count = 303005
     while count != 10000000000000000:
         await message.channel.send(str(count))
         time.sleep(1)
